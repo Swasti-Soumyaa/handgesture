906338b0feb011139156749d881768dff1bebe74
Swasti-Soumyaa <72751070+Swasti-Soumyaa@users.noreply.github.com>
2021-02-25 20:51:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function takeSnapshot(){
 
 console.log(ml5.version);
 
-classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/RFJIYzibY/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/.json",modelLoaded);
 
 function modelLoaded(){
 console.log("modelLoaded!");
@@ -32,3 +32,70 @@ function speak(){
     var utter_this=new SpeechSynthesisUtterance(speakData1+speakData2);
     synth.speak(utter_this);
 }
+
+function check(){
+    img=document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(result);
+
+        prediction_1=result[0].label;
+        prediction_2=result[1].label;
+        document.getElementById("emotion1").innerHTML=prediction_1;
+        document.getElementById("emotion2").innerHTML=prediction_2;
+        speak();
+
+        if(prediction_1=="nice"){
+            document.getElementById("emoji1").innerHTML="&#128557"
+        }
+
+        if(prediction_1=="thumbs up"){
+            document.getElementById("emoji1").innerHTML="&#128522"
+        }
+        if(prediction_1=="thumbs down"){
+            document.getElementById("emoji1").innerHTML="&#128548"
+        }
+
+        if(prediction_1=="hi"){
+            document.getElementById("emoji1").innerHTML="&#128562"
+        }
+
+        if(prediction_1=="clap"){
+            document.getElementById("emoji1").innerHTML="&#128532"
+        }
+
+        if(prediction_2=="victory"){
+            document.getElementById("emoji2").innerHTML="&#128532"
+        }
+
+
+        if(prediction_2=="thumbs up"){
+            document.getElementById("emoji2").innerHTML="&#128557"
+        }
+
+        if(prediction_2=="thumbs down"){
+            document.getElementById("emoji2").innerHTML="&#128522"
+        }
+        if(prediction_2=="hi"){
+            document.getElementById("emoji2").innerHTML="&#128548"
+        }
+
+        if(prediction_2=="clap"){
+            document.getElementById("emoji2").innerHTML="&#128562"
+        }
+
+        if(prediction_2=="victory"){
+            document.getElementById("emoji2").innerHTML="&#128532"
+        }
+
+    
+
+    }
+}
+
